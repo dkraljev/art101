4fa390fadf5038ac1f4bc00e7e227a3ce4148586
dkraljev <77074807+dkraljev@users.noreply.github.com>
2021-01-27 17:50:28 -0800

    Lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+//
+//Author: Danielle Kraljevski
+//Created: 1/26/2021
+//Public Domain
+//
+
+//Define Variables
+make = "Honda";
+model = "Civic";
+color = "Rusty";
+year = 2019;
+
+//calculate
+age = 2021 - year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
