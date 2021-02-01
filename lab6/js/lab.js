bd561ccd178b5d7d8c5b839b58d0f86e9834085a
dkraljev <77074807+dkraljev@users.noreply.github.com>
2021-02-01 10:11:15 -0800

    Lab 6

---
+++ lab6/js/lab.js
@@ -0,0 +1,23 @@
+/*
+*Author Danielle Kraljevski
+*Created 1/29/2021
+*License: Public Domain
+*/
+
+//Variable Define
+myTransport = ["civic","skateboard", "bicycle"]
+
+myMainRide = {
+  make: "Honda",
+  model: "Civic",
+  color: "Black",
+  year: 2019,
+  age: function(){
+    return 2021 - age;
+  }
+
+}
+
+//outputt
+document.writeLn("Kinds of Transportation I have Access to: ", MyTransport, "</br>")
+document.writeLn("My Main Ride: ", JSON.stringify(myMainRide, null, '\t'), "</br>")
