11e316d68962f29efa51507ef8e251cfd1b69513
dkraljev <77074807+dkraljev@users.noreply.github.com>
2021-02-03 20:43:22 -0800

    lab 7

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -1,23 +1,19 @@
 /*
 *Author Danielle Kraljevski
-*Created 1/29/2021
+*Created 2/2/2021
 *License: Public Domain
 */
 
-//Variable Define
-myTransport = ["civic","skateboard", "bicycle"]
-
-myMainRide = {
-  make: "Honda",
-  model: "Civic",
-  color: "Black",
-  year: 2019,
-  age: function(){
-    return 2021 - age;
-  }
+//Function Define
+//split string, sort it, rejoin it
+function sortName(userName) {
+  var arrayName = userName.split('').sort().join('');
+  return arrayName;
 
 }
 
-//outputt
-document.writeLn("Kinds of Transportation I have Access to: ", MyTransport, "</br>")
-document.writeLn("My Main Ride: ", JSON.stringify(myMainRide, null, '\t'), "</br>")
+var name = prompt("What is your name?");
+console.log("Old name:", name);
+ // Output
+ document.writeln("Your name sorted is :",
+sortName(name), "</br>");
