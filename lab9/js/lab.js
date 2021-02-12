8693c035f7105fae1e1322784b2978bc9b0bbaa6
dkraljev <77074807+dkraljev@users.noreply.github.com>
2021-02-12 14:59:44 -0800

    Update lab.js

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -1,35 +1,9 @@
-<<<<<<< HEAD
 /*
-=======
-*
->>>>>>> b0398c5d3099fd4433696e6f403db27b47db06b2
 * Author: Zhendong Jiang & Danielle Kraljevski
 * Created: 2/10/2021
 * License: Public Domain
 */
 
-<<<<<<< HEAD
-//Create a new element with document.createElement("p") and assign it to a variable new1El
-//Change the html attribute of new1El to say something new.
-var outputEl = document.getElementById("output");
-var new1El = document.createElement("p");
-//variable outputEl
-
-//new1El = "This is an example of me changing this element to say something new. Using Javascript I can
-//hange what elements say using"
-
-//Create another new element and assign it to a variable new2El
-//Change the html attribute of new2El to say something else.
-var new2El = document.createElement("div");
-new2El.innerHTML = "<p>It is not so hard to change the div once you have created a new element using Java!</p>"
-
-//Append both new elements one at a time using appendChild() to your output <div>
-//Change the css attributes of at least two elements of your page
-outputEl.appendChild(new2El);
-
-document.writeln(new1El);
-//HELP IM BAD
-=======
 // Use getElementById() to find your output <div> and assign it to a variable outputEl
 var outputEl = document.getElementById("output");
 
@@ -37,7 +11,7 @@ var outputEl = document.getElementById("output");
 var new1El = document.createElement("p");
 
 // Change the html attribute of new1El to say something new.
-new1El.innerHTML = "<p>This is an example of me changing this element to say something new.</p>"
+new1El.innerHTML = "This is an example of me changing this element to say something new."
 
 // Create another new element and assign it to a variable new2El
 var new2El = document.createElement("div");
@@ -66,4 +40,3 @@ function clickMe() {
 function changeBack() {
   window.location.reload();
 }
->>>>>>> b0398c5d3099fd4433696e6f403db27b47db06b2
