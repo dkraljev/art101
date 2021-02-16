206e623d2a4ca7e3dd970c9cca7aa7b42e34abc2
dkraljev <77074807+dkraljev@users.noreply.github.com>
2021-02-15 20:59:14 -0800

    lab10.2

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -3,37 +3,23 @@
 * Created: 2/14/2021
 * License: Public Domain
 */
-
 //Function Define
 //split string, sort it, rejoin it
 function sortName(userName) {
-  // split string to array
-  var strArray = userName.split(',');
-  console.log("strArray =", strArray);
-  // sort the array
-  var arraySort = strArray.sort();
-  console.log("arraySort =", arraySort);
-  // join array back to string
-  var strSort = arraySort.join('');
-  console.log("strSort =", strSort);
-  return strSort;
-
-  var button = document.getElementById('my-button');
-  button.addEventListener('click', function() {
-      inputValue = document.getElementById('user-name').value;
-      console.log("You input:", inputValue);
-  });
+  return userName.toLowerCase().split("").sort().join("");
 }
 
-buttonEl.addEventListener("click", function(){
-  var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
-  console.log("user name:", userName)
-});
+var button = document.getElementById('my-button');
+    button.addEventListener("click", function() {
+        // get the element and its value (user's name)
+        var userName = document.getElementById('user-name').value;
+        // modify the name
+        var sortedName = sortName(userName);
 
-// a button that let you try again new input
+        var outputEl = document.getElementById('output');
+        outputEl.innerHTML = sortName(userName);
+      });
+
 function tryAgain() {
   window.location.reload();
 }
-//
-// Output
-document.writeln("Fixed! Here's your name: ", sortName(), "</br>");
